50b3836898447748b747657c30baed3f28fa294d
Felix <felixsilva109@outlook.com>
2023-03-24 15:59:38 -0700

    Revert "debt form category complete #19"
    This reverts commit c09b9e7194bfb1c0f48f58fe1fe5dca908506224.

--- DebtSupervisor/addTransaction/forms.py
+++ DebtSupervisor/addTransaction/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from addTransaction.models import expenseEntry, expenseCategory
 from dataclasses import fields
+from tkinter import Widget
 from django.core import validators
 from django.forms import CharField, ModelForm, Textarea
 from django.contrib.auth.models import User
--- DebtSupervisor/app1/models.py
+++ DebtSupervisor/app1/models.py
@@ -4,34 +4,12 @@ import math
 # Create your models here.
 
 class Debtentry(models.Model):
-    CREDIT_CARD = 'Credit Card'
-    AUTO_LOAN = 'Auto Loan'
-    PERSONAL_LOAN = 'Personal Loan'
-    STUDENT_LOAN = 'Student Loan'
-    MORTGAGE = 'Mortgage'
-    MEDICAL_LOAN = 'Medical Loan'
-    TAXES = 'Taxes'
-    OTHER = 'Other'
-
-    DEBT_TYPES = [
-        (CREDIT_CARD, 'Credit Card'),
-        (AUTO_LOAN, 'Auto Loan'),
-        (PERSONAL_LOAN, 'Personal Loan'),
-        (STUDENT_LOAN, 'Student Loan'),
-        (MORTGAGE, 'Mortgage'),
-        (MEDICAL_LOAN, 'Medical Loan'),
-        (TAXES, 'Taxes'),
-        (OTHER, 'Other'),
-    ]
-
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    type = models.CharField(max_length=255, choices=DEBT_TYPES)
-    name = models.CharField(max_length=128)
     currBalance = models.FloatField()
     TotalBalance = models.FloatField()
-    apr = models.FloatField()
-    minPayment = models.FloatField()
-    dueDate = models.DateField()
-
+    Name = models.CharField(max_length=128)
+    AprRate = models.FloatField()
+    transactionDate = models.DateField()
+   
     def getProgress(self):
         return math.ceil((self.currBalance/self.TotalBalance)*100)
